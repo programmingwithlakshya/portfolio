7ffda25ee02795d59d9ea79ac35d2f7b5fd41cef
programmingwithlakshya <lakshyabca2021@gmail.com>
2025-01-16 11:29:33 +0530

    responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,45 +4,46 @@ function Header() {
   return (
         <div className='head'>
             <div className='fl'>
-                <a className="nav-link" href="#page1"><h3>Laksh</h3></a>
+                                <button className="nav-link" href="#page1"><h3>Laksh</h3></button> 
+                                <button type="button" className="btn btn-primaryz bg5 h24" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Hire Me</button>
+                                <button className="btn btn-secondary dropdown-toggle h24" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    More
+                                </button>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="#page1">Home</a></li>
+                                    <li><a className="dropdown-item" href="#page2">About</a></li>
+                                    <li><a className="dropdown-item" href="#page3">Skills</a></li>
+                                    <li><a className="dropdown-item" href="#page5">Project</a></li>
+                                    <li><a className="dropdown-item" href="#page4">Contact</a></li>
+                                </ul>
+                               
+                                
             </div>
             <div className='fr'>
                     <ul className="nav justify-content-end">
                 <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#page1">Home</a>
+                    <a className="nav-link active h21" aria-current="page" href="#page1">Home</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="#page2">About</a>
+                    <a className="nav-link h21" href="#page2">About</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="#page3">Skills</a>
+                    <a className="nav-link h21" href="#page3">Skills</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="#page5">Projects</a>
+                    <a className="nav-link h21" href="#page5">Projects</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="#page4">Contact</a>
+                    <a className="nav-link h21" href="#page4">Contact</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="model1" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Hire Me</a>
+                    <a className="nav-link h21" href="model1" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Hire Me</a>
                 </li>
-                <div className="dropdown">
-                    <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        more
-                    </button>
-                    <ul className="dropdown-menu">
-                        <li><a className="dropdown-item" href="#page1">Home</a></li>
-                        <li><a className="dropdown-item" href="#page2">ABout</a></li>
-                        <li><a className="dropdown-item" href="#page3">Skills</a></li>
-                        <li><a className="dropdown-item" href="#page5">Project</a></li>
-                        <li><a className="dropdown-item" href="#page4">Contact</a></li>
-                        <li><a className="dropdown-item" href="difhfh" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Hire Me</a></li>
-                    </ul>
-                </div>
-                {/* <button type="button" className="btn btn-primaryz" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Hire Me</button> */}
-                </ul>
-
                 
+                 
+                
+                {/* <button type="button" className="btn btn-primaryz" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Hire Me</button> */}
+                </ul>          
 
                 
                 
--- src/components/Page4.js
+++ src/components/Page4.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Hire from './Hire'
 
 
 function Page4() {
